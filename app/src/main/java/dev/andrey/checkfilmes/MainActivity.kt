0db5ee8880326a5eda26d248872d481f3b93022a
andrey <andreywill9@gmail.com>
2022-05-18 19:10:23 -0300

    Commit Inicial

---
+++ app/src/main/java/dev/andrey/checkfilmes/MainActivity.kt
@@ -0,0 +1,11 @@
+package dev.andrey.checkfilmes
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
