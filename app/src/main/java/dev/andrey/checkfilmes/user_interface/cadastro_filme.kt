fdb2a340a365af71c1bf5195ad6b36a53e25e305
andrey <andreywill9@gmail.com>
2022-05-24 19:30:27 -0300

    Criação de tela para novo filme

---
+++ app/src/main/java/dev/andrey/checkfilmes/user_interface/cadastro_filme.kt
@@ -0,0 +1,12 @@
+package dev.andrey.checkfilmes.user_interface
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import dev.andrey.checkfilmes.R
+
+class cadastro_filme : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cadastro_filme)
+    }
+}
