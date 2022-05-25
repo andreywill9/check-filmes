2376b24c9b2de129790fa5c59f509a9d8b986e8f
andrey <andreywill9@gmail.com>
2022-05-24 21:24:59 -0300

    Ajustes finais layout do cadastro de filme e validação dos campos

--- app/src/main/java/dev/andrey/checkfilmes/user_interface/CadastroFilmeActivity.kt
+++ app/src/main/java/dev/andrey/checkfilmes/user_interface/CadastroFilmeActivity.kt
@@ -2,11 +2,37 @@ package dev.andrey.checkfilmes.user_interface
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import dev.andrey.checkfilmes.R
+import dev.andrey.checkfilmes.databinding.ActivityCadastroFilmeBinding
 
 class CadastroFilmeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityCadastroFilmeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_cadastro_filme)
+        binding = ActivityCadastroFilmeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnNovoFilme.setOnClickListener {
+            if (!validarCampos()) return@setOnClickListener
+            cadastrarFilme()
+        }
+    }
+
+    private fun validarCampos(): Boolean {
+        if (binding.edtNomeFilme.text.isNullOrEmpty()) {
+            Toast.makeText(this, "Informe o nome do filme", Toast.LENGTH_LONG).show()
+            return false
+        }
+        if (binding.switchJaAssisti.isActivated && (binding.edtData.text.isNullOrEmpty())) {
+            Toast.makeText(this, "Informe quando assistiu o filme", Toast.LENGTH_LONG).show()
+            return false
+        }
+        return true
+    }
+
+    private fun cadastrarFilme() {
+
     }
 }
--- app/src/main/java/dev/andrey/checkfilmes/user_interface/ListaFilmesActivity.kt
+++ app/src/main/java/dev/andrey/checkfilmes/user_interface/ListaFilmesActivity.kt
@@ -1,12 +1,22 @@
 package dev.andrey.checkfilmes.user_interface
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import dev.andrey.checkfilmes.R
+import dev.andrey.checkfilmes.databinding.ActivityCadastroFilmeBinding
+import dev.andrey.checkfilmes.databinding.ActivityListaFilmesBinding
 
 class ListaFilmesActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityListaFilmesBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_lista_filmes)
+        binding = ActivityListaFilmesBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.btnAbrirCadastro.setOnClickListener {
+            startActivity(Intent(applicationContext, CadastroFilmeActivity::class.java))
+        }
     }
 }
